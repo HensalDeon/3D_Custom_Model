5ece128be14c93fe9d8837428e8b29959c524059
hensal <hensaldeon777@gmail.com>
2023-12-04 19:08:19 +0530

    comments

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Customizer from "./pages/Customizer";
 import Home from "./pages/Home";
 function App() {
     return (
+        // try adding error boundaries for optimal perfomance
         <main className="app transition-all ease-in">
             <Home />
             <Canvas />
